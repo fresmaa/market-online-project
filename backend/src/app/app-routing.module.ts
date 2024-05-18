54eb37f03bb41cf65c3c49ce91b663b83a2d294c
fresmaa <fresmaa@thebigrichgroup.com>
2024-05-19 02:23:41 +0700

    feat(login): add auth

--- backend/src/app/app-routing.module.ts
+++ backend/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'auth',
     loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule),
   },
   {
@@ -14,11 +14,13 @@ const routes: Routes = [
     path: 'errors',
     loadChildren: () => import('./modules/error/error.module').then((m) => m.ErrorModule),
   },
-  { path: '**', redirectTo: 'errors/404' },
+  { path: '**', redirectTo: 'auth' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- backend/src/app/modules/auth/pages/sign-in/sign-in.component.ts
+++ backend/src/app/modules/auth/pages/sign-in/sign-in.component.ts
@@ -4,6 +4,8 @@ import { Router, RouterLink } from '@angular/router';
 import { NgClass, NgIf } from '@angular/common';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { environment } from 'src/environments/environment';
+import axios from 'axios';
 
 @Component({
   selector: 'app-sign-in',
@@ -13,40 +15,72 @@ import { ButtonComponent } from '../../../../shared/components/button/button.com
   imports: [FormsModule, ReactiveFormsModule, RouterLink, AngularSvgIconModule, NgClass, NgIf, ButtonComponent],
 })
 export class SignInComponent implements OnInit {
-  form!: FormGroup;
+  formLogin!: FormGroup;
   submitted = false;
   passwordTextType!: boolean;
+  errorMessage:string = '';
 
-  constructor(private readonly _formBuilder: FormBuilder, private readonly _router: Router) {}
-
-  onClick() {
-    console.log('Button clicked');
-  }
+  constructor(
+    private readonly _formBuilder: FormBuilder,
+    private readonly _router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.form = this._formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
+    this.formLogin = this._formBuilder.group({
+      username: ['', [Validators.required]],
       password: ['', Validators.required],
     });
   }
 
   get f() {
-    return this.form.controls;
+    return this.formLogin.controls;
   }
 
   togglePasswordTextType() {
     this.passwordTextType = !this.passwordTextType;
   }
 
-  onSubmit() {
+  async login() {
     this.submitted = true;
-    const { email, password } = this.form.value;
+    this.errorMessage = '';
+
+    const { username, password } = this.formLogin.value;
 
     // stop here if form is invalid
-    if (this.form.invalid) {
+    if (this.formLogin.invalid) {
       return;
     }
 
-    this._router.navigate(['/']);
+    try {
+      const res = await axios.post(`${environment.api_url}/auth/login`, {
+        username: username,
+        password: password,
+      }, {
+        headers: {
+          Accept: 'application/json',
+          "Content-Type": 'application/json'
+        }
+      });
+      const data = await res.data;
+      if (!data.status) {
+        this.errorMessage = data.error_message;
+        window.scrollTo(0,0);
+
+        setTimeout(() => {
+          this.errorMessage = '';
+        }, 1500);
+
+        return;
+      }
+
+      const token = data.token;
+      window.localStorage.setItem(environment.api_token_identifier, token);
+      console.log('mausk')
+      this._router.navigateByUrl('/dashboard');
+      console.log('mausk2')
+    } catch (error) {
+      console.log(error);
+      this.errorMessage = 'Sorry, something went wrong. Please try again later!';
+    }
   }
 }
